dae7740c433a054d5e0737f4e8d441a27fca9994
Otaviano Manoel <otavianomanoelvasconcelos@gmail.com>
2024-06-22 20:22:40 -0300

    Inicio do repositorio

---
+++ src/Component/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import '../css/reset.css'
+import logo from '../images/logo.svg'
+
+function App() {
+  return (
+    <div className="App">
+      <img className='logo' src={logo} alt="Logo" />
+    </div>
+  );
+}
+
+export default App;
